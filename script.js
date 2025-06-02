847022dca7e2cd0d577a4d378eea3e92fd58cd7e
kratmilan <milankratochvill10@gmail.com>
2025-06-02 14:42:14 +0200

    vegso allapot, billentyu funkciokkal, es par hibakezelessel

--- script.js
+++ script.js
@@ -18,19 +18,43 @@ function show(data) {
     contents.textContent = data
     display.appendChild(contents) 
 }
-
+let isnegative = false
 function operate(calcul,data) {
     let current = 0
     let isnumber = false
     if (data >=0 && data<=9) {
         isnumber = true
     }
-    if (calcul.a == "x"  && isnumber) {
+    if (data == "-" && calcul.a =="x") {
+        isnegative = true
+        current = "-"
+    }
+    else if (calcul.a == "x"  && isnumber) {
         calcul.a = data
+        if (isnegative) {
+            calcul.a = -calcul.a
+            isnegative = false
+        }
         current = calcul.a
     }
+    else if (data == "backspace") {
+        if (calcul.b != "x") {
+            calcul.b = Math.floor(calcul.b / 10)
+            current = calcul.b
+        }
+        else if (calcul.oper != "0") {
+            calcul.oper = "0"
+            current = "0"
+        }
+        else if (calcul.a != "x") {
+            calcul.a = Math.floor(calcul.a / 10)
+            current = calcul.a
+        }
+        else current = 0
+    }
     else if (calcul.a !="x" && calcul.oper == "0" && isnumber) {
-        calcul.a = calcul.a * 10 + data
+        if (calcul.a <0) calcul.a = calcul.a * 10 - data
+        else calcul.a = calcul.a * 10 + data
         current = calcul.a
     }
     else if ((isnumber == false) && data != "=" && data != "cl") {
@@ -41,14 +65,34 @@ function operate(calcul,data) {
         if (calcul.b != "x") {
             calcul.b = calcul.b * 10 + data
         }
+        
         else calcul.b = data
         current = calcul.b
     }
     else if (data == "=") {
-        if (calcul.oper == "+") current = calcul.a + calcul.b
+        if (calcul.oper == "/" && calcul.b == 0) {
+            current = "Math error"
+            show(current)
+        }
+        else if (calcul.oper == "+") current = calcul.a + calcul.b
         else if (calcul.oper == "-") current = calcul.a - calcul.b
         else if (calcul.oper == "*") current = calcul.a * calcul.b
         else if (calcul.oper == "/") current = calcul.a / calcul.b
+        if (calcul.b == "x") current = calcul.a
+
+        if  (current != "Math error") {
+        calcul.a = "x"
+        calcul.oper = "0"
+        calcul.b = "x"
+        } else {
+            calcul.a = "x"
+            calcul.oper = "0"
+            calcul.b = "x"
+        }
+        if (Math.floor(current) == current) {
+            current = current.toFixed(0)
+        }
+        else current = current.toFixed(2)
     }
     else if (data == "cl") {
         calcul.a = "x"
@@ -56,7 +100,7 @@ function operate(calcul,data) {
         calcul.b = "x"
         current = 0
     }
-
+    
     show(current)
 }
 
@@ -108,3 +152,36 @@ buttoneq.onclick = () => operate(calc,"=")
 
 const buttoncl = document.querySelector("#cl")
 buttoncl.onclick = () => operate(calc,"cl")
+
+
+
+document.addEventListener('keydown', function(event) {
+    const output = document.getElementById('output')
+    if (event.key >= '0' && event.key <= '9') {
+        operate(calc, parseInt(event.key))
+    }
+    else if (event.key === '+') {
+        operate(calc, "+")
+    }
+    else if (event.key === '-') {
+        operate(calc, "-")
+    }
+    else if (event.key === '*') {
+        operate(calc, "*")
+    }
+    else if (event.key === '/') {
+        operate(calc, "/")
+    }
+    else if (event.key === '=') {
+        operate(calc, "=")
+    }
+    else if (event.key === 'Enter') {
+        operate(calc, "=")
+    }
+    else if (event.key === 'c' || event.key === 'C') {
+        operate(calc, "cl")
+    }
+    else if (event.key == 'Backspace') {
+        operate(calc,"backspace")
+    }
+})
